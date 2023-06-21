143aeac0e85c2b0bf49d525929b43ebe2a2c1d9f
Evertonildo Maia <evertonildo@gmail.com>
2023-06-21 11:28:14 -0300

    aaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,27 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDeGa77cF7ULwOjMfdGb9RCaDz27kktXMs",
+  authDomain: "market-place-world-e545a.firebaseapp.com",
+  projectId: "market-place-world-e545a",
+  storageBucket: "market-place-world-e545a.appspot.com",
+  messagingSenderId: "676652785321",
+  appId: "1:676652785321:web:3069e257ab8577db64d3b8",
+  measurementId: "G-QLNWFZEFXH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @NgModule({
   imports: [
